8877c49c3bc18485d842b9cb4225653e07a5a54a
kobelchal@gmail.com <35@dmin@.Net45>
2022-10-02 16:50:16 +0300

    added interrupts for ADS

--- LCDShield_SI5351/LCDShield_SI5351.ino
+++ LCDShield_SI5351/LCDShield_SI5351.ino
@@ -5,64 +5,102 @@
 */
 #include <LiquidCrystal.h>
 
-
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-float freq = 10000; // Оглашение переменных -- Частота
+enum BUTTON:char { btnRIGHT, btnUP, btnDOWN, btnLEFT, btnSELECT, btnNONE };
+
+volatile BUTTON btnValue = BUTTON::btnNONE;
+volatile bool newValue;
+volatile bool converted;
+float freq = 1000000; // Оглашение переменных -- Частота
 float bigStep = 1000; // Шаг изменения частоты при нажатии вверх/вниз
 float littleStep = 10; // Шаг изменения частоты при нажатии вправо/влево 
-int lcd_key = 0;
-int adc_key_in = 0;
-#define btnRIGHT  0 // Создание директив для кнопок
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
-#define btnSELECT 4
-#define btnNONE   5
 
-int read_LCD_buttons() { // Функция считывания нажатия кнопок
-	adc_key_in = analogRead(0);
-	if (adc_key_in > 1000) return btnNONE;
-	if (adc_key_in < 50)   return btnRIGHT;
-	if (adc_key_in < 150)  return btnUP;
-	if (adc_key_in < 315)  return btnDOWN;
-	if (adc_key_in < 600)  return btnLEFT;
-	if (adc_key_in < 850)  return btnSELECT;
-	return btnNONE;
+inline BUTTON read_LCD_buttons(int value);
+inline void setupAnalogInterrupt();
+void printValue();
+void sendFrequency(float freq);
+void PrintFrequency(float freq);
+
+// Interrupt service routine for the ADC completion
+ISR(ADC_vect) {
+
+	BUTTON value = read_LCD_buttons(ADC);
+
+	if (btnValue != value) {
+
+		btnValue = value;
+		newValue = true;
+	}
+	converted = true;
 }
 
-void sendFrequency(double frequency) { // Преобразование и отправка 
+inline void setupAnalogInterrupt() {
 
+	//  ADC Setup
+	ADCSRA = 0b10001111;      // C1:: Enable ADC & Interrupt, pre-scaler = 128
+	ADMUX = 0b01000000;      // C2:: Use AVCC and A0 as input to ADC
+	sei();                    // C3:: Set global interrupt flag
+
+	converted = true;
+	newValue = false;
+}
+
+void sendFrequency(float freq) {
 
 }
 
 void setup() {
-
+	//Serial.begin(9600);
 	lcd.begin(16, 2); // Старт библиотеки. Указанием количества символов и строк
+	setupAnalogInterrupt();
+	updateData();
 }
 
-// the loop function runs over and over again until power down or reset
+
 void loop() {
-  
-	lcd.setCursor(0, 0); // Далее вывод текущего значения частоты
-	lcd.print("Freq: ");
-	lcd.setCursor(6, 0);
-	lcd.print("           ");
-	lcd.setCursor(6, 0);
-	if (freq < 1000) {
-		lcd.print(freq);
-		lcd.print("Hz");
+
+	unsigned long mil = 0;
+
+	if (newValue || mil + 1000 < millis()) {
+
+		updateData();
+		newValue = false;
+		mil = millis();
+		delay(100);             // C5:: give you time to see the ouput
 	}
-	if ((freq >= 1000) && (freq < 1000000)) {
-		lcd.print(freq / 1000);
-		lcd.print("kHz");
+
+	if (converted) {
+
+		ADCSRA |= bit(ADSC) | bit(ADIE);   // C4:: start converting voltage on A0
+		converted = false;
 	}
-	if ((freq >= 1000000) && (freq < 50000000)) {
-		lcd.print(freq / 1000000);
-		lcd.print("MHz");
-	}
+
+	delay(200); // Пауза 200 мс
+}
+inline BUTTON read_LCD_buttons(int value) { // Функция считывания нажатия кнопок
+
+	if (value > 1000) return BUTTON::btnNONE;
+	if (value < 50)   return BUTTON::btnRIGHT;
+	if (value < 150)  return BUTTON::btnUP;
+	if (value < 315)  return BUTTON::btnDOWN;
+	if (value < 600)  return BUTTON::btnLEFT;
+	if (value < 850)  return BUTTON::btnSELECT;
+	return BUTTON::btnNONE;
+}
+
+
+void printValue() {
+
+	static char* arr[] = { "RIGHT", "UP", "DOWN", "LEFT", "SELECT", "NONE" };
+
 	lcd.setCursor(0, 1);
-	lcd.print("   Genie v1.0");
+	lcd.print("        ");
+	lcd.setCursor(0, 1);
+	lcd.print(arr[btnValue]);
+}
+
+inline void updateStep() {
 	if (freq < 100) {
 		bigStep = 10; // Определение шага грубой и точной
 		littleStep = 1;
@@ -91,31 +129,61 @@ void loop() {
 		bigStep = 10000000;
 		littleStep = 100000;
 	}
-	lcd_key = read_LCD_buttons();  // Считывание клавиш
-	switch (lcd_key) // Далее обработка нажатий клавиш          
+}
+
+inline void updateFrequncy(BUTTON btn) {
+
+
+	switch (btn) // Далее обработка нажатий клавиш          
 	{
 	case btnRIGHT:
-	{freq += littleStep;
-	break; }
+		freq += littleStep;
+		break;
 	case btnLEFT:
-	{freq -= littleStep;
-	break; }
+		freq -= littleStep;
+		break;
 	case btnUP:
-	{freq += bigStep;
-	break; }
+		freq += bigStep;
+		break;
 	case btnDOWN:
-	{if (freq == bigStep) { freq -= (bigStep / 10); }
-	else { freq -= bigStep; }
-	break; }
+		freq == bigStep ? freq -= (bigStep / 10) : freq -= bigStep;
+		break;
 	case btnSELECT:
-	{break; }
+		break;
 	case btnNONE:
-	{break; }
+		break;
 	}
 	if (freq < 1) freq = 1; // Ограничение значений частоты
 	if (freq > 40000000) freq = 40000000;
-	
-	sendFrequency(freq);  // Вызов функции отправки частоты
-	
-	delay(200); // Пауза 200 мс
+
 }
+
+void updateData() {
+	updateStep();
+	updateFrequncy(btnValue);
+	sendFrequency(freq);  // Вызов функции отправки частоты
+	PrintFrequency(freq);
+}
+
+void PrintFrequency(float freq) {
+
+	lcd.setCursor(0, 0); // Далее вывод текущего значения частоты
+	lcd.print("Freq: ");
+	lcd.setCursor(6, 0);
+	lcd.print("           ");
+	lcd.setCursor(6, 0);
+	if (freq < 1000) {
+		lcd.print(freq);
+		lcd.print("Hz");
+	}
+	if ((freq >= 1000) && (freq < 1000000)) {
+		lcd.print(freq / 1000);
+		lcd.print("kHz");
+	}
+	if ((freq >= 1000000) && (freq < 50000000)) {
+		lcd.print(freq / 1000000);
+		lcd.print("MHz");
+	}
+}
+
+
